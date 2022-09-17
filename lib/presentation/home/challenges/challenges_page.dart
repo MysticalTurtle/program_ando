515e1eca8586cd86f44d5c7ce70b21df1a5fbd27
rodrigopradoc <rodrigo070998.5@hotmail.com>
2022-09-16 23:37:05 -0500

    Agregado 6 niveles de categorias

--- lib/presentation/home/challenges/challenges_page.dart
+++ lib/presentation/home/challenges/challenges_page.dart
@@ -18,6 +18,7 @@ class ChallengesPage extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisSize: MainAxisSize.min,
               children: [
+                //NIVEL 1
                 const SizedBox(height: 12),
                 SingleChildScrollView(
                   child: Row(
@@ -27,13 +28,51 @@ class ChallengesPage extends StatelessWidget {
                         heightImage: 180,
                         imageProvider: AssetImage('assets/category/if.jpg'),
                         tags: [_tag('Condicionales', () {})],
-                        title: _title1(),
-                        description: _content1(),
-                        footer: _footer1(),
+                        title: _title('IF'),
+                        description: _content(
+                            'Conoce el condicional IF. ¿Cumple con la condición? ejecuta la acción'),
+                        footer: _footer('Nivel 1'),
                       ),
                     ],
                   ),
                 ),
+                //NIVEL 2
+                const SizedBox(height: 12),
+                SingleChildScrollView(
+                  child: Row(
+                    children: [
+                      FillImageCard(
+                        width: 350,
+                        heightImage: 180,
+                        imageProvider: AssetImage('assets/category/ifelse.jpg'),
+                        tags: [_tag('Condicionales', () {})],
+                        title: _title('IF-ELSE'),
+                        description: _content(
+                            '¿No se cumple la condición? añade otra opción'),
+                        footer: _footer('Nivel 2'),
+                      ),
+                    ],
+                  ),
+                ),
+                //NIVEL 3
+                const SizedBox(height: 12),
+                SingleChildScrollView(
+                  child: Row(
+                    children: [
+                      FillImageCard(
+                        width: 350,
+                        heightImage: 180,
+                        imageProvider: AssetImage('assets/category/switch.jpg'),
+                        tags: [_tag('Condicionales', () {})],
+                        title: _title('SWITCH'),
+                        description: _content(
+                            '¿Muchas opciones? define sus instrucciones de antemano'),
+                        footer: _footer('Nivel 3'),
+                      ),
+                    ],
+                  ),
+                ),
+                //NIVEL 4
                 const SizedBox(height: 12),
                 SingleChildScrollView(
                   child: Row(
@@ -43,9 +82,47 @@ class ChallengesPage extends StatelessWidget {
                         heightImage: 180,
                         imageProvider: AssetImage('assets/category/loop.jpg'),
                         tags: [_tag('Bucles', () {})],
-                        title: _title2(),
-                        description: _content2(),
-                        footer: _footer2(),
+                        title: _title('FOR'),
+                        description: _content(
+                            'Conoce las estructuras de control ¿Cuántas veces repetir una acción?'),
+                        footer: _footer('Nivel 4'),
+                      ),
+                    ],
+                  ),
+                ),
+                //NIVEL 5
+                const SizedBox(height: 12),
+                SingleChildScrollView(
+                  child: Row(
+                    children: [
+                      FillImageCard(
+                        width: 350,
+                        heightImage: 180,
+                        imageProvider: AssetImage('assets/category/while.png'),
+                        tags: [_tag('Bucles', () {})],
+                        title: _title('WHILE'),
+                        description: _content(
+                            '¡Ejecuta una instrucción mientras se cumpla la condición!'),
+                        footer: _footer('Nivel 5'),
+                      ),
+                    ],
+                  ),
+                ),
+                //NIVEL 6
+                const SizedBox(height: 12),
+                SingleChildScrollView(
+                  child: Row(
+                    children: [
+                      FillImageCard(
+                        width: 350,
+                        heightImage: 180,
+                        imageProvider:
+                            AssetImage('assets/category/dowhile.png'),
+                        tags: [_tag('Bucles', () {})],
+                        title: _title('DO WHILE'),
+                        description: _content(
+                            'Ejecuta la instrucción al menos una vez ¡no importa si no cumple alguna condición!'),
+                        footer: _footer('Nivel 6'),
                       ),
                     ],
                   ),
@@ -56,35 +133,20 @@ class ChallengesPage extends StatelessWidget {
     ));
   }
 
-  Widget _title1({Color? color}) {
+  Widget _title(titulo) {
     return Text(
-      'IF & IF-ELSE',
-      style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600, color: color),
+      titulo,
+      style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
     );
   }
 
-  Widget _title2({Color? color}) {
+  Widget _content(descripcion) {
     return Text(
-      'For & While',
-      style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600, color: color),
+      descripcion,
     );
   }
 
-  Widget _content1({Color? color}) {
-    return Text(
-      'Conoce las sentencias condicionales. ¡Tú eliges qué hacer!',
-      style: TextStyle(color: color),
-    );
-  }
-
-  Widget _content2({Color? color}) {
-    return Text(
-      'Conoce las estructuras de control. ¿Cúantas veces repetir una acción?',
-      style: TextStyle(color: color),
-    );
-  }
-
-  Widget _footer1({Color? color}) {
+  Widget _footer(nivel) {
     return Row(
       children: [
         CircleAvatar(
@@ -98,30 +160,7 @@ class ChallengesPage extends StatelessWidget {
         ),
         Expanded(
             child: Text(
-          'Nivel 1',
-          style: TextStyle(color: color),
-        )),
-        IconButton(onPressed: () {}, icon: Icon(Icons.play_circle))
-      ],
-    );
-  }
-
-  Widget _footer2({Color? color}) {
-    return Row(
-      children: [
-        CircleAvatar(
-          backgroundImage: AssetImage(
-            'assets/category/estrella.png',
-          ),
-          radius: 12,
-        ),
-        const SizedBox(
-          width: 4,
-        ),
-        Expanded(
-            child: Text(
-          'Nivel 2',
-          style: TextStyle(color: color),
+          nivel,
         )),
         IconButton(onPressed: () {}, icon: Icon(Icons.play_circle))
       ],
